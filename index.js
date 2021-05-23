412ac3cc7be47f4aaf97f1f9661acfb00c49545c
Srinivas Natarajan <nsrinivas06@gmail.com>
2021-05-23 15:15:01 +0530

    attached NN

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.get("/", (req, res) => {
   
   var er = process.stderr.toString()
   if (er) { console.log(er)}
+  result = process.stdout.toString().trim()
+  console.log("Loading: \n", result)
 
   res.sendFile(__dirname + "/views/index.html");
 });
@@ -27,15 +29,18 @@ app.post('/', function(req, res){
   console.log(req.body.inputEmail);
   console.log(req.body.inputPassword);
   var spawnSync = require("child_process").spawnSync
-  var process = spawnSync('D:/Anaconda/envs/ISM/python',["./ml/prediction.py",'sample@gmail.com',"admin' or 1 = 1#"])  
+  var process = spawnSync('D:/Anaconda/envs/ISM/python',["./ml/prediction.py",req.body.inputEmail,req.body.inputPassword])  
   
   var er = process.stderr.toString()
   if (er) { console.log(er)}
 
   result = process.stdout.toString().trim()
-  console.log(result)
-
-  res.sendFile(__dirname + "/views/success.html");
+  console.log("Result: ", result)
+  console.log(typeof result)
+  if(result.includes("0.0"))
+    res.sendFile(__dirname + "/views/success.html");
+  else  
+  res.sendFile(__dirname + "/views/fail.html");
 });
 
 app.listen(3000);
--- ml/preload.py
+++ ml/preload.py
@@ -1,4 +1,6 @@
-import pandas as pd
+import tensorflow as tf
+tf.autograph.set_verbosity(1)
+import time
 import pickle
 from keras.models import load_model
 from sklearn.feature_extraction.text import CountVectorizer
@@ -8,7 +10,7 @@ print('Loading Model')
 
 ###########################################
 
-
+start = time.time()
 feature_path = './ml/features.pkl'
 vectorizer = CountVectorizer(vocabulary=pickle.load(open(feature_path, "rb")))
 #posts = vectorizer.fit_transform(df['Sentence'].values.astype('U')).toarray()
@@ -18,5 +20,6 @@ model = load_model("./ml/model_NN.h5")
 model.compile(loss='binary_crossentropy', 
               optimizer='adam', 
               metrics=['accuracy'])
-
+end = time.time()
+print("Time taken: ", round(end-start,3), "seconds")
 
