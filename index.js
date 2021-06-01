9f5a8b625a85f64ca043edf71f6dd66785bf8e2c
Srinivas Natarajan <nsrinivas06@gmail.com>
2021-06-01 09:22:34 +0530

    CSS added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var express = require("express");
+var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var async = require('async');
+var path = require('path')
+
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
@@ -8,10 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+app.use(express.static(__dirname + '/public'));
 
 
 app.get("/", (req, res) => {
-
+  /*
   var spawnSync = require("child_process").spawnSync
   var process = spawnSync('D:/Anaconda/envs/ISM/python',["./ml/preload.py"])  
   
@@ -19,17 +23,21 @@ app.get("/", (req, res) => {
   if (er) { console.log(er)}
   result = process.stdout.toString().trim()
   console.log("Loading: \n", result)
-
+  */
   res.sendFile(__dirname + "/views/index.html");
 });
 
 
-app.post('/', function(req, res){
+app.get('/form', function(req, res){
 
-  console.log(req.body.inputEmail);
-  console.log(req.body.inputPassword);
+  var email = req.query.inputEmail
+  var pwd = req.query.inputPassword
+
+  console.log("Email: ",email);
+  console.log("Password: ",pwd);
+
   var spawnSync = require("child_process").spawnSync
-  var process = spawnSync('D:/Anaconda/envs/ISM/python',["./ml/prediction.py",req.body.inputEmail,req.body.inputPassword])  
+  var process = spawnSync('D:/Anaconda/envs/ISM/python',["./ml/prediction.py",email,pwd])  
   
   var er = process.stderr.toString()
   if (er) { console.log(er)}
@@ -43,4 +51,6 @@ app.post('/', function(req, res){
   res.sendFile(__dirname + "/views/fail.html");
 });
 
-app.listen(3000);
+app.listen(3000, function() { //Set port to listen to
+  console.log('Server running at http://127.0.0.1:3000/');
+  });
